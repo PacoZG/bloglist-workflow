91afbc3cfdd2fd020e914915f639dda7b28f6ff9
Francisco Zavala <64326252+PacoZG@users.noreply.github.com>
2021-03-10 11:37:15 +0200

    Update blog_api.test.js

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -178,54 +178,54 @@ describe('since we have few blos saved', () => {
   })
 })
 
-describe('since we have few users saved in our data base', () => {
-  beforeEach(async () => {
-    await User.deleteMany({})
-    const passwordHash = await bcrypt.hash('sekret',10)
-    const user = new User({ username: 'rootUser', name: 'user', passwordHash })
-    await user.save()
-  })
+// describe('since we have few users saved in our data base', () => {
+//   beforeEach(async () => {
+//     await User.deleteMany({})
+//     const passwordHash = await bcrypt.hash('sekret',10)
+//     const user = new User({ username: 'rootUser', name: 'user', passwordHash })
+//     await user.save()
+//   })
 
-  test('we succesfully create a new user', async () => {
-    const usersAtStart = await helper.usersInDb()
+//   test('we succesfully create a new user', async () => {
+//     const usersAtStart = await helper.usersInDb()
 
-    const newUser = {
-      username: 'sirpako',
-      name: 'Francisco Zavala',
-      password: 'superSecret',
-    }
+//     const newUser = {
+//       username: 'sirpako',
+//       name: 'Francisco Zavala',
+//       password: 'superSecret',
+//     }
 
-    await api
-      .post('/api/users').send(newUser)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
+//     await api
+//       .post('/api/users').send(newUser)
+//       .expect(201)
+//       .expect('Content-Type', /application\/json/)
 
-    const usersAtEnd = await helper.usersInDb()
-    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+//     const usersAtEnd = await helper.usersInDb()
+//     expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
 
-    const usernames = usersAtEnd.map(u => u.username)
-    expect(usernames).toContain(newUser.username)
-  })
+//     const usernames = usersAtEnd.map(u => u.username)
+//     expect(usernames).toContain(newUser.username)
+//   })
 
-  test('we cannot create new user when a username already exists in the data base', async () => {
-    const usersAtStart = await helper.usersInDb()
+//   test('we cannot create new user when a username already exists in the data base', async () => {
+//     const usersAtStart = await helper.usersInDb()
 
-    const newUser = {
-      username: 'rootUser',
-      name: 'Superuser',
-      password: 'password',
-    }
+//     const newUser = {
+//       username: 'rootUser',
+//       name: 'Superuser',
+//       password: 'password',
+//     }
 
-    const result = await api.post('/api/users').send(newUser)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
+//     const result = await api.post('/api/users').send(newUser)
+//       .expect(400)
+//       .expect('Content-Type', /application\/json/)
 
-    expect(result.body.error).toContain('`username` to be unique')
+//     expect(result.body.error).toContain('`username` to be unique')
 
-    const usersAtEnd = await helper.usersInDb()
-    expect(usersAtEnd).toHaveLength(usersAtStart.length)
-  })
-})
+//     const usersAtEnd = await helper.usersInDb()
+//     expect(usersAtEnd).toHaveLength(usersAtStart.length)
+//   })
+// })
 
 
 afterAll(() => { mongoose.connection.close() })
